6e57f151f8c011b7ed26b20d65e96eb4566af3bc
AurySepe <aureliosepe01@gmail.com>
2022-07-20 17:45:19 +0200

    fixed sounds

--- Utility/AudioResources.swift
+++ Utility/AudioResources.swift
@@ -12,10 +12,10 @@ import RealityKit
 class AudioResources
 {
     static var collisionSound : AudioResource = try! AudioFileResource.load(named: "/Sound/hit.mp3", in: nil, inputMode: .spatial, loadingStrategy: .preload, shouldLoop: false)
-    static var goalSound : AudioResource = try! AudioFileResource.load(named: "Sound/score.mp3", in: nil, inputMode: .spatial, loadingStrategy: .preload, shouldLoop: false)
-    static var goalSoundCheering : AudioResource = try! AudioFileResource.load(named: "Sound/score-crowd.mp3", in: nil, inputMode: .spatial, loadingStrategy: .preload, shouldLoop: false)
-    static var winGame : AudioResource = try! AudioFileResource.load(named: "Sound/win.mp3", in: nil, inputMode: .spatial, loadingStrategy: .preload, shouldLoop: false)
-    static var lostGame : AudioResource = try! AudioFileResource.load(named: "Sound/lost.mp3", in: nil, inputMode: .spatial, loadingStrategy: .preload, shouldLoop: false)
-    static var setDown : AudioResource = try! AudioFileResource.load(named: "Sound/setdown.mp3", in: nil, inputMode: .spatial, loadingStrategy: .preload, shouldLoop: false)
-    static var background : AudioResource = try! AudioFileResource.load(named: "Sound/background.mp3", in: nil, inputMode: .spatial, loadingStrategy: .preload, shouldLoop: false)
+    static var goalSound : AudioResource = try! AudioFileResource.load(named: "/Sound/score.mp3", in: nil, inputMode: .spatial, loadingStrategy: .preload, shouldLoop: false)
+    static var goalSoundCheering : AudioResource = try! AudioFileResource.load(named: "/Sound/score-crowd.mp3", in: nil, inputMode: .spatial, loadingStrategy: .preload, shouldLoop: false)
+    static var winGame : AudioResource = try! AudioFileResource.load(named: "/Sound/win.mp3", in: nil, inputMode: .spatial, loadingStrategy: .preload, shouldLoop: false)
+    static var lostGame : AudioResource = try! AudioFileResource.load(named: "/Sound/lost.mp3", in: nil, inputMode: .spatial, loadingStrategy: .preload, shouldLoop: false)
+    static var setDown : AudioResource = try! AudioFileResource.load(named: "/Sound/setdown.mp3", in: nil, inputMode: .spatial, loadingStrategy: .preload, shouldLoop: false)
+    static var background : AudioResource = try! AudioFileResource.load(named: "/Sound/background.mp3", in: nil, inputMode: .spatial, loadingStrategy: .preload, shouldLoop: false)
 }
